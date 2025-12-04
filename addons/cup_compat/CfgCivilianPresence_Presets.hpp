372390a7ac23087fb33e75f9fa8a550e185f680e
Andy <andx667@protonmail.com>
2025-12-04 10:03:43 +0100

    init

---
+++ addons/cup_compat/CfgCivilianPresence_Presets.hpp
@@ -0,0 +1,35 @@
+class CivilianPresence_Presets {
+    //https://community.bistudio.com/wiki/Arma_3:_Civilian_Presence
+    // class CIV_3CB_Factions_ADC {
+    //     name = "Men (African Desert Civilians (3CB Factions))";
+    //     picture = "UK3CB_Factions\addons\UK3CB_Factions_ADC\Flag\ADC_flag_co.paa";
+    //     value = "CIV_3CB_Factions_ADC";
+    //     defaultValue = "CIV_3CB_Factions_ADC";
+
+    //     class UnitTypes {
+    //         bis_units[] = {
+    //             "UK3CB_ADC_C_CIV_CHR_01",
+    //             "UK3CB_ADC_C_CIV_CHR_02",
+    //             "UK3CB_ADC_C_HUNTER_CHR",
+    //             "UK3CB_ADC_C_CIV_ISL_01",
+    //             "UK3CB_ADC_C_CIT",
+    //             "UK3CB_ADC_C_COACH",
+    //             "UK3CB_ADC_C_PROF",
+    //             "UK3CB_ADC_C_VILL",
+    //             "UK3CB_ADC_C_WOOD",
+    //             "UK3CB_ADC_C_HELIPILOT",
+    //             "UK3CB_ADC_C_PILOT",
+    //             "UK3CB_ADC_C_DOC_CHR",
+    //             "UK3CB_ADC_C_DOC_ISL",
+    //             "UK3CB_ADC_C_FUNC",
+    //             "UK3CB_ADC_C_LABOURER_CHR",
+    //             "UK3CB_ADC_C_LABOURER_ISL",
+    //             "UK3CB_ADC_C_CIV_CHR",
+    //             "UK3CB_ADC_C_CIV_ISL",
+    //             "UK3CB_ADC_C_SPOT_CHR",
+    //             "UK3CB_ADC_C_SPOT_ISL",
+    //             "UK3CB_ADC_C_WORKER"
+    //         };
+    //     };
+    // };
+};
---
+++ addons/cup_compat/CfgVehicles.hpp
@@ -0,0 +1,4 @@
+class CfgVehicles {
+    //African Desert Civilians
+    //CREATE_AGENT(UK3CB_ADC_C_CIV_CHR_01);
+};
