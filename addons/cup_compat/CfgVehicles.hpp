1634c151cf69f132bbf982000be1c2f1a937420f
Andy <andx667@protonmail.com>
2025-12-04 16:38:20 +0100

    add terrain support template

--- addons/cup_compat/CfgVehicles.hpp
+++ addons/cup_compat/CfgVehicles.hpp
@@ -1,4 +1,20 @@
 class CfgVehicles {
-    //African Desert Civilians
+    //Create new Agent Classes for Civlian Units
     //CREATE_AGENT(UK3CB_ADC_C_CIV_CHR_01);
+
+    //Add Unittypes to terrains
+    class Module_F;
+    class ModuleCivlianPresence_F: Module_F {
+        class UnitTypes {
+            chernarus[] = {
+            //     "C_Man_1_enoch_F",
+            //     "C_Man_2_enoch_F",
+            //     "C_Man_3_enoch_F",
+            //     "C_Man_4_enoch_F",
+            //     "C_Man_5_enoch_F",
+            //     "C_Man_6_enoch_F",
+            //     "C_Farmer_01_enoch_F"
+            };
+        };
+    };
 };
